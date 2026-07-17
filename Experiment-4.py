8d73f481444157da130c2545160d161931dfe129
Rajasekhar-1111 <192424216.simats@saveetha.com>
2026-07-17 09:44:12 +0530

    Add files via upload

---
+++ Experiment-4.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+sales_data = np.array([10000, 15000, 18000, 20000])
+
+total_sales = np.sum(sales_data)
+
+percentage_increase = ((sales_data[3] - sales_data[0]) / sales_data[0]) * 100
+
+print("Total Sales:", total_sales)
+print("Percentage Increase:", percentage_increase, "%")
